9fcc2000546645cb29bc4d7eb45d31124f432c8d
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-01-31 14:13:24 -0300

    chore: configurate ChakraUI fonts and colors

---
+++ src/styles/theme.ts
@@ -0,0 +1,30 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "50": "#fAfAfA",
+      "100": "#F4F4F5",
+      "200": "#E4E4E7",
+      "300": "#D4D4D8",
+      "400": "#A1A1AA",
+      "500": "#71717A",
+      "600": "#52525B",
+      "700": "#3F3F46",
+      "800": "#27272A",
+      "900": "#18181B",
+    },
+  },
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.900",
+        color: "gray.50",
+      },
+    },
+  },
+});
