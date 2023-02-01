9d58e90064be1965937a63ff534f39d49788eee6
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-01 17:48:15 -0300

    feat: configurate mirage lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
 import { SidebarDrawerProvider } from "@/contexts/SidebarDrawerContext";
+import { makeServer } from "@/services/mirage";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
