adba3164837e4c8ea97058aaba2e60e4514b8981
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-02-01 18:43:41 -0300

    feat: show loading when refetch data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -33,24 +33,27 @@ interface User {
 }
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(["users"], async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-    const users = data.users?.map((user: User) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
+  const { data, isLoading, isFetching, error } = useQuery(
+    ["users"],
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+      const users = data.users?.map((user: User) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -68,6 +71,9 @@ export default function UserList() {
           <Flex mb={"8"} justify={"space-between"} align={"center"}>
             <Heading size={"lg"} fontWeight={"normal"}>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size={"sm"} color={"gray.500"} ml={"4"} />
+              )}
             </Heading>
 
             <Button
